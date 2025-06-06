ea42c85e47c5a11a403f7bd9732534b10488dab0
SergeyFedos <feddoseev@gmail.com>
2025-06-06 08:46:58 +0000

    finished project

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ function isValidMoves(x, y) {
     [x + 2, y - 1],
     [x - 2, y + 1],
     [x - 2, y - 1]
-  ]
-  return moves.filter(([x, y]) => x >= 0 && y < 8 && y >= 0 && x < 8);
+  ];
+  return moves.filter(([newX, newY]) => newX >= 0 && newX < 8 && newY >= 0 && newY < 8);
 }
+
+function knightMoves(start, end) {
+  let queue = [[start]];
+  let seen = new Set();
+
+  while (queue.length) {
+    let path = queue.shift();
+    let [currentX, currentY] = path[path.length - 1];
+
+    if (currentX === end[0] && currentY === end[1]) {
+      console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+      return path;
+    }
+
+    const key = `${currentX},${currentY}`;
+    if (seen.has(key)) continue;
+    seen.add(key);
+
+    for (let move of isValidMoves(currentX, currentY)) {
+      queue.push([...path, move]);
+    }
+  }
+
+  return null;
+}
+console.log(knightMoves([0, 0], [7, 7]))
